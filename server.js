5b3dc13a652b6f1ae919fca86ecb6574da493fae
writatas <jnelson.writing@gmail.com>
2021-11-20 10:04:49 +0100

    Figured out that logicscript.js couldn't read the MIME type of game_objects because the module reference didn't have the .js extension name?

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const {port} = require('./config')
 
 app.use('/modules',express.static(__dirname + '/modules'))
@@ -9,6 +8,7 @@ app.use('/Public',express.static(__dirname + '/Public'))
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/views/hash.html')
 })
+
 app.listen(port,()=>{
     console.log(`Example app listening at http://localhost:${port}`)
 })
