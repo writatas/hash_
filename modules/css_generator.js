3e7456e05cd4642827a40ecd02e2a13ac7efc87b
writatas <jnelson.writing@gmail.com>
2022-01-02 18:50:13 +0100

    worked on implementing CSS generator, decided to predefine css which will be executed depending on running variable, opposed to using Mutations observers

--- modules/css_generator.js
+++ modules/css_generator.js
@@ -1,5 +1,14 @@
-const CSS_Generate = ()=> (function(){
-    const [usr_cmds,usr_info,plW]
-
-})()
+const CSS_Generate = (...arr)=> (function(...cssArr){
+    arr = cssArr
+    const [player,enemy_que,clock] = [...arr]
+    //Access dom elements and change by comparing the current dom value with the actual running values.
+    const usr_cmds = document.getElementById('USER_commands')
+    const CSS = {
+        user_commands : `
+            border:2px solid black;
+            width:100%`,
+        
+    }
+    usr_cmds.style = CSS.user_commands
+})(...arr)
 export {CSS_Generate}
--- modules/html_generator.js
+++ modules/html_generator.js
@@ -4,6 +4,7 @@ const HTML_Generate_Objects = (...arr) => (function(...generate_array){
     const  [player,enemy_que,clock] = [...arr]
     
     //PLAYER
+    //These variables are used to check the condition of the HTML
     let cmds,pn, w, n, l, p, eq, inv, enc
     cmds = document.getElementById("USER_commands")
     pn = document.getElementById("USER_info")
@@ -20,9 +21,9 @@ const HTML_Generate_Objects = (...arr) => (function(...generate_array){
     //USER COMMANDS (executed externally through the combat.js module)
         let command_node = document.createElement("div")
         let user_cmds = document.createElement("textarea")
-
+        user_cmds.style.width = "100%" // rewrite this to the css_generator once you figure out how to change childrens css again
         command_node.id = "USER_commands"
-        command_node.appendChild("user_cmds")
+        command_node.appendChild(user_cmds)
 
     //PLAYER INFO:
         let player_node,
