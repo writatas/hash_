4e69661342737a03f4bce4e67ec78dcbe0d671f8
writatas <jnelson.writing@gmail.com>
2022-04-07 19:54:04 +0200

    adjusting the attack power of enemies

--- modules/game_objects.js
+++ modules/game_objects.js
@@ -160,7 +160,7 @@ const Game = (hero,level) => (function(hero_name,starting_level)
                 else
                 {
                     //updates via while loop in the logic script
-                    player._ouch = player.level * this.perception
+                    player._ouch = Math.floor((this.perception *  player.level) / 2)
                 }
                 
             }
